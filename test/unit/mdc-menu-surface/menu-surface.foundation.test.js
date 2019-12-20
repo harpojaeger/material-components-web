c5dcfdae8e73c4f2d18e0edaf45ab9f83f282ebf
Rafael Harpo Jaeger <harpo@google.com>
2019-12-20 16:17:10 -0500

    chore: extract some size constants to make test assertions more readable

--- test/unit/mdc-menu-surface/menu-surface.foundation.test.js
+++ test/unit/mdc-menu-surface/menu-surface.foundation.test.js
@@ -44,19 +44,26 @@ function setupTest() {
   return {foundation, mockAdapter};
 }
 
+const defaultWindowHeight = 1000;
+const defaultWindowWidth = 1000;
+const defaultMenuSurfaceHeight = 200;
+const defaultMenuSurfaceWidth = 100;
+const anchorHeight = 20;
+
 // Various anchor dimensions.
-const smallTopLeft = {height: 20, width: 40, top: 20, bottom: 40, left: 20, right: 60};
-const smallTopRight = {height: 20, width: 40, top: 20, bottom: 40, left: 920, right: 960};
-const smallBottomLeft = {height: 20, width: 40, top: 920, bottom: 940, left: 20, right: 60};
-const smallBottomRight = {height: 20, width: 40, top: 920, bottom: 940, left: 920, right: 960};
-const smallCenter = {height: 20, width: 40, top: 490, bottom: 510, left: 480, right: 520};
-const smallAboveMiddleLeft = {height: 20, width: 40, top: 400, bottom: 420, left: 20, right: 60};
-const smallBelowMiddleLeft = {height: 20, width: 40, top: 600, bottom: 620, left: 20, right: 60};
-const wideCenter = {height: 20, width: 150, top: 490, bottom: 510, left: 450, right: 600};
-const wideTopLeft = {height: 20, width: 150, top: 20, bottom: 40, left: 20, right: 170};
+const smallTopLeft = {height: anchorHeight, width: 40, top: 20, bottom: 40, left: 20, right: 60};
+const smallTopRight = {height: anchorHeight, width: 40, top: 20, bottom: 40, left: 920, right: 960};
+const smallBottomLeft = {height: anchorHeight, width: 40, top: 920, bottom: 940, left: 20, right: 60};
+const smallBottomRight = {height: anchorHeight, width: 40, top: 920, bottom: 940, left: 920, right: 960};
+const smallCenter = {height: anchorHeight, width: 40, top: 490, bottom: 510, left: 480, right: 520};
+const smallAboveMiddleLeft = {height: anchorHeight, width: 40, top: 400, bottom: 420, left: 20, right: 60};
+const smallBelowMiddleLeft = {height: anchorHeight, width: 40, top: 600, bottom: 620, left: 20, right: 60};
+const wideCenter = {height: anchorHeight, width: 150, top: 490, bottom: 510, left: 450, right: 600};
+const wideTopLeft = {height: anchorHeight, width: 150, top: 20, bottom: 40, left: 20, right: 170};
+
 
 /**
- * Initializes viewport, anchor and menu surface dimensions. Viewport is 1000x1000. Default surface size is 100x200.
+ * Initializes viewport, anchor and menu surface dimensions.
  * @param {!MDCMenuSurfaceAdapter} mockAdapter Mock double for the adapter.
  * @param {!ClientRect} anchorDimensions Approximate viewport corner where anchor is located.
  * @param {boolean=} isRtl Indicates whether layout is RTL.
@@ -64,13 +71,13 @@ const wideTopLeft = {height: 20, width: 150, top: 20, bottom: 40, left: 20, righ
  * @param {!MDCMenuPoint=} scrollValue Optional scroll values of the page.
  */
 function initAnchorLayout(mockAdapter, anchorDimensions, isRtl = false,
-  menuSurfaceHeight = 200, scrollValue = {x: 0, y: 0}) {
+  menuSurfaceHeight = defaultMenuSurfaceHeight, scrollValue = {x: 0, y: 0}) {
   td.when(mockAdapter.hasAnchor()).thenReturn(true);
-  td.when(mockAdapter.getWindowDimensions()).thenReturn({height: 1000, width: 1000});
+  td.when(mockAdapter.getWindowDimensions()).thenReturn({height: defaultWindowHeight, width: defaultWindowWidth});
   td.when(mockAdapter.getAnchorDimensions()).thenReturn(anchorDimensions);
   td.when(mockAdapter.isRtl()).thenReturn(isRtl);
-  td.when(mockAdapter.getInnerDimensions()).thenReturn({height: menuSurfaceHeight, width: 100});
-  td.when(mockAdapter.getBodyDimensions()).thenReturn({height: 1000, width: 1000});
+  td.when(mockAdapter.getInnerDimensions()).thenReturn({height: menuSurfaceHeight, width: defaultMenuSurfaceWidth});
+  td.when(mockAdapter.getBodyDimensions()).thenReturn({height: defaultWindowHeight, width: defaultWindowWidth});
   td.when(mockAdapter.getWindowScroll()).thenReturn(scrollValue);
 }
 
